74a57acfff2d073911f6c454f0faae9b9e0dd72d
AminBhst <amin.bhst@gmail.com>
2023-03-11 17:57:33 +0330

    add generated settings adapter

---
+++ lib/model/setting.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'setting.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SettingAdapter extends TypeAdapter<Setting> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Setting read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Setting(
+      name: fields[0] as String,
+      value: fields[1] as String,
+      settingType: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Setting obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.value)
+      ..writeByte(2)
+      ..write(obj.settingType);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
