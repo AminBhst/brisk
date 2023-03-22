6c4b4e0a06d389ec4f0f997c6c4d3da315ec2b71
AminBhst <amin.bhst@gmail.com>
2023-03-22 16:36:09 +0430

    minor change

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,6 @@ import flutter_local_notifications
 import hotkey_manager
 import path_provider_foundation
 import screen_retriever
-import sqflite
-import sqlite3_flutter_libs
 import url_launcher_macos
 import window_manager
 
@@ -19,8 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   HotkeyManagerPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,6 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   hotkey_manager
   screen_retriever
-  sqlite3_flutter_libs
   url_launcher_windows
   window_manager
   windows_notification
