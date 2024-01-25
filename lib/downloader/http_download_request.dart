77139c3ea6ce5937599a79da803615c932b5453d
AminBhst <amin.bhst@gmail.com>
2024-01-25 18:16:28 +0330

    Add User Agent to headers

--- lib/downloader/http_download_request.dart
+++ lib/downloader/http_download_request.dart
@@ -141,7 +141,9 @@ class HttpDownloadRequest {
   }
 
   http.Request buildDownloadRequest() {
-    return http.Request('GET', Uri.parse(downloadItem.downloadUrl));
+    final request = http.Request('GET', Uri.parse(downloadItem.downloadUrl));
+    request.headers.addAll({"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko;"});
+    return request;
   }
 
   void sendDownloadRequest(http.Request request) {
