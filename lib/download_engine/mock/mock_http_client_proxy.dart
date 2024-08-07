264046a421c57f50fb10edc801c58f69cf9f72de
AminBhst <amin.bhst@gmail.com>
2024-08-07 18:27:13 +0330

    Add doc

--- lib/download_engine/mock/mock_http_client_proxy.dart
+++ lib/download_engine/mock/mock_http_client_proxy.dart
@@ -17,7 +17,7 @@ String testFilePath = path.join(
 );
 
 /// A mock http client that serves a local file while mimicking the volatility
-/// of a real network  connection. This proxy object is used to develop and test
+/// of a real network connection. This proxy object is used to develop and test
 /// the download engine without making real http connections.
 class MockHttpClientProxy implements BaseClient {
   late Client client;
