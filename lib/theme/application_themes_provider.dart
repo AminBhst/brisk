23f214e615fc72c3ebdd4f787eeb2875635917f1
AminBhst <amin.bhst@gmail.com>
2024-10-13 01:46:20 +0330

    Delete unused file

--- lib/theme/application_themes_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:brisk/theme/application_theme.dart';
-import 'package:flutter/cupertino.dart';
-
-class ApplicationThemesProvider with ChangeNotifier {
-  // ApplicationTheme activeTheme;
-
-
-
-}
