669b8efb948cb13d34e67696048a2fb05ad0b8a4
AminBhst <amin.bhst@gmail.com>
2024-10-20 23:32:11 +0330

    Add checkbox provider to top menu to prevent unacceptable pause/start commands

---
+++ lib/widget/top_menu/top_menu_util.dart
@@ -0,0 +1,29 @@
+import 'package:brisk/db/hive_util.dart';
+import 'package:brisk/download_engine/download_status.dart';
+import 'package:brisk/provider/download_request_provider.dart';
+import 'package:brisk/provider/pluto_grid_util.dart';
+
+bool isDownloadButtonEnabled(DownloadRequestProvider provider) {
+  final selectedRowIds = PlutoGridUtil.selectedRowIds;
+  final completedDownloadSelected = selectedRowIds
+      .map((id) => HiveUtil.instance.downloadItemsBox.get(id))
+      .toList()
+      .any((download) =>
+          download != null &&
+          download.status == DownloadStatus.assembleComplete);
+  return (selectedRowIds.isEmpty || completedDownloadSelected)
+      ? false
+      : (provider.downloads.values
+              .where((item) => selectedRowIds.contains(item.downloadItem.id))
+              .every((item) => item.buttonAvailability.startButtonEnabled) ||
+          provider.downloads.values.isEmpty);
+}
+
+bool isPauseButtonEnabled(DownloadRequestProvider provider) {
+  final selectedRowIds = PlutoGridUtil.selectedRowIds;
+  return (selectedRowIds.isEmpty || provider.downloads.values.isEmpty)
+      ? false
+      : provider.downloads.values
+          .where((item) => selectedRowIds.contains(item.downloadItem.id))
+          .every((item) => item.buttonAvailability.pauseButtonEnabled);
+}
