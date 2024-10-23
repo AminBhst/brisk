8439b4124cf6b5948294ec3ee34885ae72630a37
AminBhst <amin.bhst@gmail.com>
2024-10-23 18:23:43 +0330

    Add hotkey

--- lib/util/hot_key_util.dart
+++ lib/util/hot_key_util.dart
@@ -1,21 +1,21 @@
-// import 'package:clipboard/clipboard.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/services.dart';
-// import 'package:hotkey_manager/hotkey_manager.dart';
-//
-// import 'download_addition_ui_util.dart';
-//
-// void registerDefaultDownloadAdditionHotKey(BuildContext context) {
-//   final _hotkey = HotKey(
-//     key: LogicalKeyboardKey.keyA,
-//     modifiers: [HotKeyModifier.control, HotKeyModifier.alt],
-//     scope: HotKeyScope.inapp,
-//   );
-//   hotKeyManager.register(
-//     _hotkey,
-//     keyDownHandler: (hotKey) async {
-//       String url = await FlutterClipboard.paste();
-//       DownloadAdditionUiUtil.handleDownloadAddition(context, url);
-//     },
-//   );
-// }
+import 'package:clipboard/clipboard.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
+
+import 'download_addition_ui_util.dart';
+
+void registerDefaultDownloadAdditionHotKey(BuildContext context) {
+  final _hotkey = HotKey(
+    key: LogicalKeyboardKey.keyA,
+    modifiers: [HotKeyModifier.control, HotKeyModifier.alt],
+    scope: HotKeyScope.inapp,
+  );
+  hotKeyManager.register(
+    _hotkey,
+    keyDownHandler: (hotKey) async {
+      String url = await FlutterClipboard.paste();
+      DownloadAdditionUiUtil.handleDownloadAddition(context, url);
+    },
+  );
+}
