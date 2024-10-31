60e4ac0dbab06b7397ba23bb1353138acc6de9e4
AminBhst <amin.bhst@gmail.com>
2024-10-31 21:31:34 +0330

    Ignore updater files during update

--- brisk_auto_updater/lib/downloader/update_downloader.dart
+++ brisk_auto_updater/lib/downloader/update_downloader.dart
@@ -103,7 +103,8 @@ class UpdateDownloader {
           : TarDecoder().decodeBytes(XZDecoder().decodeBytes(package));
 
       for (final file in archive) {
-        if (file.name.contains("updater")) {
+        if (file.name.startsWith("updater/") ||
+            file.name.startsWith("updater\\")) {
           continue;
         }
         var filename = join(
@@ -179,6 +180,7 @@ class UpdateDownloader {
     try {
       response
           .asStream()
+          .timeout(Duration(seconds: 10))
           .listen((streamedResponse) {
         try {
           final headers = streamedResponse.headers;
