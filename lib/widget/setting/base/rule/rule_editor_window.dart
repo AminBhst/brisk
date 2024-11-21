8b78b02aa81c3f5ef127f6d7ad5e9e2c531bac88
AminBhst <amin.bhst@gmail.com>
2024-11-21 16:02:13 +0330

    Add responsiveness to rule editor window

--- lib/widget/setting/base/rule/rule_editor_window.dart
+++ lib/widget/setting/base/rule/rule_editor_window.dart
@@ -1,8 +1,7 @@
 import 'package:brisk/provider/theme_provider.dart';
-import 'package:brisk/util/settings_cache.dart';
 import 'package:brisk/widget/base/closable_window.dart';
+import 'package:brisk/widget/base/rounded_outlined_button.dart';
 import 'package:brisk/widget/setting/base/rule/rule_item_editor.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,9 +13,10 @@ class RuleEditorWindow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeProvider>(context).activeTheme;
+    final size = MediaQuery.of(context).size;
     return ClosableWindow(
       width: 500,
-      height: 650,
+      height: size.height > 750 ? 650 : size.height * 0.8,
       backgroundColor: theme.alertDialogTheme.backgroundColor,
       content: SizedBox(
         child: Column(
@@ -31,7 +31,7 @@ class RuleEditorWindow extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             Container(
-              height: 450,
+              height: resolveScrollviewHeight(size),
               width: 400,
               decoration: BoxDecoration(
                 color: theme.settingTheme.sideMenuTheme.backgroundColor,
@@ -54,11 +54,57 @@ class RuleEditorWindow extends StatelessWidget {
                   ],
                 ),
               ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                RoundedOutlinedButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  borderColor:
+                      theme.alertDialogTheme.cancelButtonColor.borderColor,
+                  hoverTextColor:
+                      theme.alertDialogTheme.cancelButtonColor.hoverTextColor,
+                  hoverBackgroundColor: theme
+                      .alertDialogTheme.cancelButtonColor.hoverBackgroundColor,
+                  textColor: theme.alertDialogTheme.cancelButtonColor.textColor,
+                  width: 95,
+                  text: "Cancel",
+                ),
+                const SizedBox(width: 30),
+                RoundedOutlinedButton(
+                  onPressed: () {},
+                  borderColor:
+                      theme.alertDialogTheme.addButtonColor.borderColor,
+                  hoverTextColor:
+                      theme.alertDialogTheme.addButtonColor.hoverTextColor,
+                  hoverBackgroundColor: theme
+                      .alertDialogTheme.addButtonColor.hoverBackgroundColor,
+                  textColor: theme.alertDialogTheme.addButtonColor.textColor,
+                  width: 95,
+                  text: "Save",
+                )
+              ],
             )
           ],
         ),
       ),
     );
+  }
+
+  double resolveScrollviewHeight(Size size) {
+    if (size.height > 750) return 450;
+    if (size.height > 510) {
+      return size.height * (0.53 - ((750 - size.height) / 700 * 0.33));
+    }
+    if (size.height > 460) {
+      return size.height * (0.4 - ((510 - size.height) / 50 * 0.1));
+    }
+    if (size.height > 340 && size.height > 395) {
+      return size.height * (0.3 - ((460 - size.height) / 120 * 0.1));
+    }
+    return size.height * 0.1;
   }
 
   Widget ruleRow(context) {
