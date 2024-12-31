85ecf218e9aa2c1aa9c46f37288460373ad822c1
AminBhst <amin.bhst@gmail.com>
2024-12-31 23:04:35 +0330

    Change extension version

--- lib/browser_extension/browser_extension_server.dart
+++ lib/browser_extension/browser_extension_server.dart
@@ -24,7 +24,7 @@ import 'package:window_manager/window_manager.dart';
 class BrowserExtensionServer {
   static bool _isServerRunning = false;
   static bool _cancelClicked = false;
-  static const String extensionVersion = "2.0.0";
+  static const String extensionVersion = "1.2.0";
 
   static void setup(BuildContext context) async {
     if (_isServerRunning) return;
