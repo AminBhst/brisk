57ba246274fd545adb98105db78b59892d05e18a
Norman-W <85535885@qq.com>
2025-04-25 08:01:07 +0800

    #92 Refactor text rendering in RoundedOutlinedButton.
    Wrap text in a Flexible widget to improve layout handling and prevent overflow issues. Added ellipsis and disabled wrapping for better text truncation and visual consistency.

--- lib/widget/base/rounded_outlined_button.dart
+++ lib/widget/base/rounded_outlined_button.dart
@@ -97,13 +97,17 @@ class _RoundedOutlinedButtonState extends State<RoundedOutlinedButton> {
             if (widget.icon != null) widget.icon!,
             if (widget.icon != null && widget.text != null) SizedBox(width: 5),
             if (widget.text != null)
-              Text(
-                widget.text!,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: widget.hoverTextColor == null
-                      ? (hover ? Colors.white : widget.textColor)
-                      : (hover ? widget.hoverTextColor : widget.textColor),
+              Flexible(
+                child: Text(
+                  widget.text!,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: false,
+                  style: TextStyle(
+                    color: widget.hoverTextColor == null
+                        ? (hover ? Colors.white : widget.textColor)
+                        : (hover ? widget.hoverTextColor : widget.textColor),
+                  ),
                 ),
               ),
           ],
