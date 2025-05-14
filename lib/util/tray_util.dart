27afc2a9b72cf22f6e5f0ac2ea1204efe955c3ae
aminbhst <amin.bhst@gmail.com>
2025-05-14 16:02:57 +0330

    Update flatpak icon name

--- lib/util/tray_util.dart
+++ lib/util/tray_util.dart
@@ -18,7 +18,7 @@ Future<void> initTray() async {
     ],
   );
   if (isFlatpak) {
-    await trayManager.setIcon("com.aminbhst.Brisk");
+    await trayManager.setIcon("io.github.aminbhst.Brisk");
   } else {
     await trayManager.setIcon(
       Platform.isWindows ? 'assets/icons/logo.ico' : 'assets/icons/logo.png',
