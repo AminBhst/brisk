a429d809d5dbc8d6e2bd80644b12a8086a498e46
Amin Beheshti <82708492+AminBhst@users.noreply.github.com>
2025-05-17 13:07:16 +0330

    Update api address

--- brisk_auto_updater/lib/downloader/update_downloader.dart
+++ brisk_auto_updater/lib/downloader/update_downloader.dart
@@ -40,7 +40,7 @@ class UpdateDownloader {
       return;
     }
     final latestTag = await getJsonResponse(
-      "https://api.github.com/repos/AminBhst/brisk/releases/latest",
+      "https://api.github.com/repos/BrisklyDev/brisk/releases/latest",
     );
     print("god json response");
     final version = latestTag["tag_name"];
